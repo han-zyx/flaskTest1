22495724cca9e7257fe8b47ad1bc27fb49ec7b9f
Hansaka Tennakoon <hansakatennakoon4@gmail.com>
2023-03-27 19:02:55 +0530

    update16

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ def fake_news_det(news):
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
