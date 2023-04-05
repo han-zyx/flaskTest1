bce87582ac88c908a1d664864c1213beeb52c08f
Hansaka Tennakoon <90048384+han-zyx@users.noreply.github.com>
2023-04-06 05:02:33 +0530

    Flask06_04_02

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,25 @@
+var myIndex = 0;
+carousel();
 
-    // JavaScript code for the slideshow
-    const slideshow = document.querySelector('.slideshow');
-    const slides = slideshow.getElementsByTagName('img');
-    let currentSlide = 0;
-    const slideInterval = setInterval(nextSlide, 2000);
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
 
-    function nextSlide() {
-        slides[currentSlide].className = '';
-        currentSlide = (currentSlide + 1) % slides.length;
-        slides[currentSlide].className = 'active';
+let number = document.getElementById ("number");
+let counter = 0;
+setInterval(() => {
+    if (counter == 65) {
+        clearInterval();
+    } else {
+        counter += 1;
+        number.innerHTML = counter + "%"
     }
+}, 30);
