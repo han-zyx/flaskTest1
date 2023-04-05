ce3e5d6ecae21f8c9b309d773fde79bc4c2fb466
Hansaka Tennakoon <90048384+han-zyx@users.noreply.github.com>
2023-04-06 01:22:40 +0530

    imageSlide3

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,12 @@
-const slideshow = document.querySelector('.slideshow');
-const slides = slideshow.getElementsByTagName('');
-let currentSlide = 0;
-const slideInterval = setInterval(nextSlide, 2000);
 
-function nextSlide() {
-  slides[currentSlide].className = '';
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].className = 'active';
-}
+    // JavaScript code for the slideshow
+    const slideshow = document.querySelector('.slideshow');
+    const slides = slideshow.getElementsByTagName('img');
+    let currentSlide = 0;
+    const slideInterval = setInterval(nextSlide, 2000);
+
+    function nextSlide() {
+        slides[currentSlide].className = '';
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].className = 'active';
+    }
